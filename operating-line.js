bde7f4320ffc323f576fa63191e0aaea6f1ca064
cjooie <cjcalapats@gmail.com>
2024-01-20 07:11:29 +0800

    added: version 2 of operating lines

---
+++ operating-line.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "450px";
+    document.getElementById("map").style.marginLeft = "450px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("map").style.marginLeft= "0";
+}
