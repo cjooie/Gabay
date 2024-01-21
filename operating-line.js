d8234f90da7459c601c33e8fa6cc36807ae701ff
cjooie <cjcalapats@gmail.com>
2024-01-21 11:31:50 +0800

    fixed: operating line sidebar

--- operating-line.js
+++ operating-line.js
@@ -1,9 +1,47 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "450px";
-    document.getElementById("map").style.marginLeft = "450px";
+function getLocationPoints(){
+    document.getElementsByClassName("textbox");
 }
-  
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("map").style.marginLeft= "0";
+
+function interchangeLocation() {
+    var departure_value = document.getElementById("departure").value;
+    var arrival_value = document.getElementById("arrival").value;
+
+    var temp = departure_value;
+    document.getElementById("departure").value = arrival_value;
+    document.getElementById("arrival").value = temp;
+
+}
+
+function toggleSidebar() {
+    var toggle_sidebar = document.getElementById("btnControl");
+    
+    if (toggle_sidebar.checked) {
+                document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("map").style.marginLeft = "0";
+
+        var style = `
+        .sidebar-btn{
+            -webkit-transform: scaleX(1);
+            transform: scaleX(1);
+        }
+        `
+        var stylesheet = document.createElement("style");
+        stylesheet.innerText = style;
+        document.head.appendChild(stylesheet);
+    } 
+    
+    else {
+        document.getElementById("mySidebar").style.width = "450px";
+        document.getElementById("map").style.marginLeft = "450px";
+
+        var style = `
+        .sidebar-btn{
+            -webkit-transform: scaleX(-1);
+            transform: scaleX(-1);
+        }
+        `
+        var stylesheet = document.createElement("style");
+        stylesheet.innerText = style;
+        document.head.appendChild(stylesheet);
+    }
 }
